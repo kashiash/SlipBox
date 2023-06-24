b44212c0580bb76bb56d8e9df535d08c4333d334
Jacek Kosinski <kashiash@gmail.com>
2023-06-24 13:57:16 +0200

    .

--- SlipBox/Persistence.swift
+++ SlipBox/Persistence.swift
@@ -70,14 +70,6 @@ class PersistenceController {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date() + TimeInterval(index)
         }
-        do {
-            try viewContext.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
         return result
     }()
 }
