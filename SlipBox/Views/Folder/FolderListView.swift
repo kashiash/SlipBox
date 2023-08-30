46fd7ddd027b0acea8e78c1ed866bb55e423ae6e
Jacek Kosinski <kashiash@gmail.com>
2023-08-30 22:22:09 +0200

    folderListView

---
+++ SlipBox/Views/Folder/FolderListView.swift
@@ -0,0 +1,20 @@
+//
+//  FolderListView.swift
+//  SlipBox
+//
+//  Created by Jacek Kosinski U on 30/08/2023.
+//
+
+import SwiftUI
+
+struct FolderListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FolderListView_Previews: PreviewProvider {
+    static var previews: some View {
+        FolderListView()
+    }
+}
