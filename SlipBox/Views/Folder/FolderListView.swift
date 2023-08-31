2df350748c8a00ba8e1c2f27e6d9ec43a1472260
Jacek Kosinski <kashiash@gmail.com>
2023-08-31 22:45:07 +0200

    .

--- SlipBox/Views/Folder/FolderListView.swift
+++ SlipBox/Views/Folder/FolderListView.swift
@@ -12,33 +12,42 @@ struct FolderListView: View {
     @Environment(\.managedObjectContext) var context
     @FetchRequest(fetchRequest: Folder.fetch(.all)) private var folders: FetchedResults<Folder>
 
-    @State private var selectedFolder: Folder? = nil
+    @Binding var selectedFolder: Folder?
 
     var body: some View {
         List(selection: $selectedFolder) {
             ForEach(folders) { folder in
-                Text(folder.name)
-                    .tag(folder)
+                NavigationLink(value: folder) {
+                    FolderRow(folder: folder)
+                }
             }
+            .onDelete(perform: deleteFolders(offsets:))
+
         }
-        // .listStyle(.plain)
         .toolbar {
-            ToolbarItem (placement: .primaryAction)  {
+            ToolbarItem(placement: .primaryAction) {
                 Button {
-                    let newFolder = Folder(name: "new folder", context: context)
+
+                    let newFolder = Folder(name: "new Folder", context: context)
                     selectedFolder = newFolder
 
                 } label: {
                     Label("Create new folder", systemImage: "folder.badge.plus")
                 }
+
             }
         }
+    }
+    private func deleteFolders(offsets: IndexSet) {
+        offsets.map { folders[$0] }.forEach(Folder.delete(_:))
     }
 }
 
 struct FolderListView_Previews: PreviewProvider {
     static var previews: some View {
-        FolderListView()
-            .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        NavigationView{
+            FolderListView(selectedFolder: .constant(nil))
+                .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        }
     }
 }
